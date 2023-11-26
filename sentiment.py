71ac076c833949667f33a243db0ae05238191c64
hyunshi <138978567+hyunshi@users.noreply.github.com>
2023-11-26 17:20:59 +0800

    Update sentiment.py

--- sentiment.py
+++ sentiment.py
@@ -162,6 +162,14 @@ def Home():
                     df['tweets'] = df['tweets'].apply(tokenize)
                     df['tweets'].head()
 
+                    # Vectorize the text data using Word2Vec embeddings
+                    def get_word_embeddings(tokens):
+                          embeddings = [word2vec_model.wv[word] for word in tokens if word in word2vec_model.wv]
+                          return np.mean(embeddings, axis=0) if embeddings else np.zeros(word2vec_model.vector_size)
+
+                    # Apply the word embeddings to your DataFrame
+                    df['word_embeddings'] = df['tweets'].apply(get_word_embeddings)
+
 
                     # Initialize sentiment counts
                     sentiment_counts = {"Positive": 0, "Negative": 0, "Neutral": 0}
@@ -306,11 +314,11 @@ def visualize(df):
 
                             vectorizer = TfidfVectorizer(max_features=500000)
                             
-                           # Join the lists of tokens into strings
-                            df['tweets_str'] = df['tweets'].apply(lambda x: ' '.join(x))
+                            # Join the lists of tokens into strings
+                            embeddings_list = df['word_embeddings'].to_list()
 
-                            # Vectorize the text data
-                            X = vectorizer.fit_transform(df['tweets_str'])
+                            # Convert the list of arrays to a 2D NumPy array
+                            X = np.vstack(embeddings_list)
                             y = df['sentiment']
                                  
                             # Convert sentiment labels to numerical values
