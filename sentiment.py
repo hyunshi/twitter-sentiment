d0226c5a7d0745349a8f5d9c433f567f45a44270
hyunshi <138978567+hyunshi@users.noreply.github.com>
2024-01-06 19:46:08 +0800

    Update sentiment.py

--- sentiment.py
+++ sentiment.py
@@ -325,7 +325,7 @@ def visualize(df):
         y_numerical = y.map({'positive': 0, 'negative':1, 'neutral':2})
 
         # Split the data into training and testing sets
-        X_train, X_test, y_train, y_test = train_test_split(X, y_numerical, test_size=0.4, random_state=42)
+        X_train, X_test, y_train, y_test = train_test_split(X, y_numerical, test_size=0.1, random_state=42)
 
         def model_Evaluate(model):
             # Predict values for Test dataset
