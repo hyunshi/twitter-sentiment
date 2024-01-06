38f49865e0055aafcf8ab843c9701307dc209b52
hyunshi <138978567+hyunshi@users.noreply.github.com>
2024-01-06 13:43:21 +0800

    Update sentiment.py

--- sentiment.py
+++ sentiment.py
@@ -124,11 +124,12 @@ def Home():
                     #Emoji removal
                     def remove_emojis(string):
                         remove_emoji = re.compile("["u"\U0001F600-\U0001F64F"  # emoticons
-                                                u"\U0001F300-\U0001F5FF"  # symbols & pictographs
-                                                u"\U0001F680-\U0001F6FF"  # transport & map symbols
-                                                u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
-                                                "]+", flags=re.UNICODE)
-                        return remove_emoji.sub(r'', string)
+                                              u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+                                              u"\U0001F680-\U0001F6FF"  # transport & map symbols
+                                              u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+                                              "]+", flags=re.UNICODE)
+                        return remove_emoji.sub(r'', string).encode("utf-8").decode("utf-8")
+
 
                     df['tweets'] = df['tweets'].apply(remove_emojis)
 
