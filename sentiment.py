bab0499bef279bb2573a6025184421411efd5cde
hyunshi <138978567+hyunshi@users.noreply.github.com>
2024-01-06 19:48:10 +0800

    Update sentiment.py

--- sentiment.py
+++ sentiment.py
@@ -316,6 +316,8 @@ def visualize(df):
             return grid_search.best_estimator_
 
         vectorizer = TfidfVectorizer(max_features=5000)
+        max_features = vectorizer.get_feature_names_out()
+        st.write(f"TF-IDF Max Features: {max_features}")
 
         # Convert the list of arrays to a 2D NumPy array
         X = vectorizer.fit_transform(df['tweets'].apply(lambda x: ' '.join(x)))
@@ -325,7 +327,7 @@ def visualize(df):
         y_numerical = y.map({'positive': 0, 'negative':1, 'neutral':2})
 
         # Split the data into training and testing sets
-        X_train, X_test, y_train, y_test = train_test_split(X, y_numerical, test_size=0.1, random_state=42)
+        X_train, X_test, y_train, y_test = train_test_split(X, y_numerical, test_size=0.2, random_state=42)
 
         def model_Evaluate(model):
             # Predict values for Test dataset
