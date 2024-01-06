b01c4879993850b6818f37aa220a4f397d0f1f80
hyunshi <138978567+hyunshi@users.noreply.github.com>
2024-01-06 19:17:41 +0800

    Update sentiment.py

--- sentiment.py
+++ sentiment.py
@@ -328,7 +328,7 @@ def visualize(df):
         X_train, X_test, y_train, y_test = train_test_split(X, y_numerical, test_size=0.2, random_state=42)
         # Apply SMOTE for oversampling to handle imbalance
         smote = SMOTE(sampling_strategy='auto', random_state=42)
-        X_train_resampled, y_train_resampled = smote.fit_resample(X_train.apply(lambda x: ' '.join(x)), y_train)
+        X_train_resampled, y_train_resampled = smote.fit_resample(X_train, y_train)
     
         # Vectorize the resampled data
         vectorizer = TfidfVectorizer(max_features=5000)
