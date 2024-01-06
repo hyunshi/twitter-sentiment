61dfb3325b1df4b3bd0a43c8bdbe1d4aac31a648
hyunshi <138978567+hyunshi@users.noreply.github.com>
2024-01-06 18:24:43 +0800

    Update sentiment.py

--- sentiment.py
+++ sentiment.py
@@ -26,7 +26,7 @@ from tqdm import tqdm
 from wordcloud import WordCloud
 from sklearn.svm import SVC
 from sklearn.model_selection import GridSearchCV
-nltk.download('vader_lexicon')
+
 
 # Define the sid variable (SentimentIntensityAnalyzer) in the global scope
 sid = SentimentIntensityAnalyzer()
@@ -51,6 +51,7 @@ def Home():
     # Add this line to download WordNet data
     nltk.download('wordnet')
     nltk.download('stopwords')
+    nltk.download('vader_lexicon')
 
     upl = st.file_uploader('Upload file')
     if upl:
