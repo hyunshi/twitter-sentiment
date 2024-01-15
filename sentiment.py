c67d12a6ef3901e27e06b64920560c2287bbf8b8
hyunshi <138978567+hyunshi@users.noreply.github.com>
2024-01-15 23:32:09 +0800

    Update sentiment.py

--- sentiment.py
+++ sentiment.py
@@ -237,181 +237,150 @@ def Home():
             return df
 
 def visualize(df):
-         
-    # Filter tweets related to election, pru, and pilihanraya
-    election_keywords = ['general', 'pru15', 'malaysia']
-    election_related_tweets = df[df['tweets'].apply(lambda x: any(keyword in ' '.join(x) for keyword in election_keywords))]
 
-    # Check if there are positive and negative tweets related to election
-    positive_tweets_election = election_related_tweets[election_related_tweets['vader_sentiment_label'] == 'positive']['tweets']
-    negative_tweets_election = election_related_tweets[election_related_tweets['vader_sentiment_label'] == 'negative']['tweets']
+    # Training Word2Vec model
+    tokenized_tweets = df['tweets'].apply(lambda x: x.split())
+    word2vec_model = Word2Vec(sentences=tokenized_tweets, vector_size=100, window=5, min_count=1, workers=4)
+    
+    # Function to find similar words using Word2Vec embeddings
+    def find_similar_words(word, topn=5):
+        similar_words = word2vec_model.wv.most_similar(word, topn=topn)
+        return [word for word, _ in similar_words]
+    
+    # Positive and negative words from your sentiment analysis
+    positive_word = 'positive'  # replace with an actual positive word from your data
+    negative_word = 'negative'  # replace with an actual negative word from your data
+    
+    # Find similar words for positive and negative words
+    similar_positive_words = find_similar_words(positive_word)
+    similar_negative_words = find_similar_words(negative_word)
+    
+    # Create WordCloud for similar positive words
+    st.write("WordCloud for Words Similar to Positive Words:")
+    wordcloud_positive_similar = WordCloud(width=400, height=400, background_color='black').generate(' '.join(similar_positive_words))
+    image_positive_similar = wordcloud_positive_similar.to_image()
+    st.image(image_positive_similar, caption='Similar to Positive Words WordCloud', use_column_width=True)
+    
+    # Create WordCloud for similar negative words
+    st.write("WordCloud for Words Similar to Negative Words:")
+    wordcloud_negative_similar = WordCloud(width=400, height=400, background_color='black').generate(' '.join(similar_negative_words))
+    image_negative_similar = wordcloud_negative_similar.to_image()
+    st.image(image_negative_similar, caption='Similar to Negative Words WordCloud', use_column_width=True)
 
-    if not positive_tweets_election.empty and not negative_tweets_election.empty:
-        # Generate WordClouds for positive and negative sentiments
-        positive_tweets_election = ' '.join(positive_tweets_election.apply(lambda x: ' '.join(x)))
-        negative_tweets_election = ' '.join(negative_tweets_election.apply(lambda x: ' '.join(x)))
+    def tune_hyperparameters_bnb(X_train, y_train):
+        # Define the parameter grid for Bernoulli Naive Bayes
+        param_grid = {'alpha': [0.1, 0.5, 1.0, 1.5, 2.0]}
 
-        # Create a two-column layout
-        col1, col2 = st.columns(2)
+        # Create the Bernoulli Naive Bayes classifier
+        bnb_model = BernoulliNB()
 
-        # WordCloud for Positive Sentiment related to election
-        with col1:
-            st.write("WordCloud for Positive Sentiment Related to Election:")
-            wordcloud_positive_election = WordCloud(width=400, height=400, background_color='black').generate(
-                positive_tweets_election)
-            image_positive = wordcloud_positive_election.to_image()
-            st.image(image_positive, caption='Positive Sentiment WordCloud', use_column_width=True)
+        # Instantiate the GridSearchCV object
+        grid_search = GridSearchCV(estimator=bnb_model, param_grid=param_grid, scoring='accuracy', cv=5)
 
-        # WordCloud for Negative Sentiment related to election
-        with col2:
-            st.write("WordCloud for Negative Sentiment Related to Election:")
-            wordcloud_negative_election = WordCloud(width=400, height=400, background_color='black').generate(
-                negative_tweets_election)
-            image_negative = wordcloud_negative_election.to_image()
-            st.image(image_negative, caption='Negative Sentiment WordCloud', use_column_width=True)
+        # Fit the GridSearchCV to the data
+        grid_search.fit(X_train, y_train)
 
-        # Generate WordCloud based on Vader sentiment scores
-        vader_positive_tweets = election_related_tweets[election_related_tweets['vader_sentiment_label'] == 'positive'][
-            'tweets']
-        vader_negative_tweets = election_related_tweets[election_related_tweets['vader_sentiment_label'] == 'negative'][
-            'tweets']
+        return grid_search.best_estimator_
+    
+    def tune_hyperparameters_mnb(X_train, y_train):
+        # Define the parameter grid for Multinomial Naive Bayes
+        param_grid = {'alpha': [0.1, 0.5, 1.0, 1.5, 2.0]}
+    
+        # Create the Multinomial Naive Bayes classifier
+        mnb_model = MultinomialNB()
+    
+        # Instantiate the GridSearchCV object
+        grid_search = GridSearchCV(estimator=mnb_model, param_grid=param_grid, scoring='accuracy', cv=5)
+    
+        # Fit the GridSearchCV to the data
+        grid_search.fit(X_train, y_train)
+    
+        return grid_search.best_estimator_
 
-        vader_positive_tweets = ' '.join(vader_positive_tweets.apply(lambda x: ' '.join(x)))
-        vader_negative_tweets = ' '.join(vader_negative_tweets.apply(lambda x: ' '.join(x)))
+    vectorizer = TfidfVectorizer(max_features=5000000, stop_words='english', norm='l2', sublinear_tf=True)
 
-        # Create a two-column layout for Vader sentiment WordClouds
-        col3, col4 = st.columns(2)
+    # Convert the list of arrays to a 2D NumPy array
+    X = vectorizer.fit_transform(df['tweets'].apply(lambda x: ' '.join(x)))
+    y = df['vader_sentiment_label']
 
-        # WordCloud for Positive Sentiment based on Vader
-        with col3:
-            st.write("WordCloud for Positive Sentiment Based on Vader:")
-            wordcloud_positive_vader = WordCloud(width=400, height=400, background_color='black').generate(
-                vader_positive_tweets)
-            image_positive_vader = wordcloud_positive_vader.to_image()
-            st.image(image_positive_vader, caption='Positive Sentiment WordCloud (Vader)', use_column_width=True)
+    # Convert sentiment labels to numerical values
+    y_numerical = y.map({'positive': 0, 'negative':1, 'neutral':2})
 
-        # WordCloud for Negative Sentiment based on Vader
-        with col4:
-            st.write("WordCloud for Negative Sentiment Based on Vader:")
-            wordcloud_negative_vader = WordCloud(width=400, height=400, background_color='black').generate(
-                vader_negative_tweets)
-            image_negative_vader = wordcloud_negative_vader.to_image()
-            st.image(image_negative_vader, caption='Negative Sentiment WordCloud (Vader)', use_column_width=True)
+    # Split the data into training and testing sets
+    X_train, X_test, y_train, y_test = train_test_split(X, y_numerical, test_size=0.2, random_state=42)
 
-        def tune_hyperparameters_bnb(X_train, y_train):
-            # Define the parameter grid for Bernoulli Naive Bayes
-            param_grid = {'alpha': [0.1, 0.5, 1.0, 1.5, 2.0]}
-
-            # Create the Bernoulli Naive Bayes classifier
-            bnb_model = BernoulliNB()
-
-            # Instantiate the GridSearchCV object
-            grid_search = GridSearchCV(estimator=bnb_model, param_grid=param_grid, scoring='accuracy', cv=5)
-
-            # Fit the GridSearchCV to the data
-            grid_search.fit(X_train, y_train)
-
-            return grid_search.best_estimator_
-        
-        def tune_hyperparameters_mnb(X_train, y_train):
-            # Define the parameter grid for Multinomial Naive Bayes
-            param_grid = {'alpha': [0.1, 0.5, 1.0, 1.5, 2.0]}
-        
-            # Create the Multinomial Naive Bayes classifier
-            mnb_model = MultinomialNB()
-        
-            # Instantiate the GridSearchCV object
-            grid_search = GridSearchCV(estimator=mnb_model, param_grid=param_grid, scoring='accuracy', cv=5)
-        
-            # Fit the GridSearchCV to the data
-            grid_search.fit(X_train, y_train)
-        
-            return grid_search.best_estimator_
-
-        vectorizer = TfidfVectorizer(max_features=5000000, stop_words='english', norm='l2', sublinear_tf=True)
-
-        # Convert the list of arrays to a 2D NumPy array
-        X = vectorizer.fit_transform(df['tweets'].apply(lambda x: ' '.join(x)))
-        y = df['vader_sentiment_label']
+    def model_Evaluate(model):
+        # Predict values for Test dataset
+        y_pred = model.predict(X_test)
 
         # Convert sentiment labels to numerical values
-        y_numerical = y.map({'positive': 0, 'negative':1, 'neutral':2})
+        y_numerical = label_binarize(y_test, classes=[0, 1, 2])
 
-        # Split the data into training and testing sets
-        X_train, X_test, y_train, y_test = train_test_split(X, y_numerical, test_size=0.2, random_state=42)
+        # Print the evaluation metrics for the dataset.
+        classification_rep = classification_report(y_test, y_pred)
+        st.write("Classification Report:")
+        st.text(classification_rep)
 
-        def model_Evaluate(model):
-            # Predict values for Test dataset
-            y_pred = model.predict(X_test)
-
-            # Convert sentiment labels to numerical values
-            y_numerical = label_binarize(y_test, classes=[0, 1, 2])
-
-            # Print the evaluation metrics for the dataset.
-            classification_rep = classification_report(y_test, y_pred)
-            st.write("Classification Report:")
-            st.text(classification_rep)
-
-            # Compute and plot the Confusion matrix
-            cf_matrix = confusion_matrix(y_test, y_pred)
-            categories = ['Positive', 'Negative']
-            group_percentages = ['{0:.2%}'.format(value) for value in cf_matrix.flatten() / np.sum(cf_matrix)]
-            group_names = ['True Positive', 'False Positive', 'False Negative', 'True Negative']
-            labels = [f'{v1}\n{v2}' for v1, v2 in zip(group_names, group_percentages)]
-            labels = np.asarray(labels).reshape(len(categories), len(categories))
-            
-            # Display the Confusion Matrix
-            st.write("Confusion Matrix:")
-            plt.figure(figsize=(8, 6))
-            sns.heatmap(cf_matrix, annot=labels, fmt='', cmap="Blues", cbar=False,
-                        xticklabels=categories, yticklabels=categories)
-            plt.xlabel("Predicted values", fontdict={'size':14}, labelpad=10)
-            plt.ylabel("Actual values", fontdict={'size':14}, labelpad=10)
-            plt.title("Confusion Matrix", fontdict={'size':18}, pad=20)
-            st.pyplot(plt)
-
-            # Compute ROC curve and ROC area for each class
-            fpr = dict()
-            tpr = dict()
-            roc_auc = dict()
+        # Compute and plot the Confusion matrix
+        cf_matrix = confusion_matrix(y_test, y_pred)
+        categories = ['Positive', 'Negative']
+        group_percentages = ['{0:.2%}'.format(value) for value in cf_matrix.flatten() / np.sum(cf_matrix)]
+        group_names = ['True Positive', 'False Positive', 'False Negative', 'True Negative']
+        labels = [f'{v1}\n{v2}' for v1, v2 in zip(group_names, group_percentages)]
+        labels = np.asarray(labels).reshape(len(categories), len(categories))
         
-            for i in range(2):
-                fpr[i], tpr[i], _ = roc_curve(y_numerical[:, i], model.predict_proba(X_test)[:, i])
-                roc_auc[i] = auc(fpr[i], tpr[i])
-        
-            # Display the ROC curve
-            st.write("Receiver Operating Characteristic (ROC) Curve:")
-            plt.figure(figsize=(8, 6))
-        
-            for i in range(2):
-                plt.plot(fpr[i], tpr[i], label=f'Class {i} (AUC = {roc_auc[i]:.2f})')
-        
-            plt.plot([0, 1], [0, 1], 'k--', color='grey', linestyle='--', label='Random')
-            plt.xlabel('False Positive Rate')
-            plt.ylabel('True Positive Rate')
-            plt.title('ROC Curve')
-            plt.legend(loc='lower right')
-            plt.grid(True)
-            
-            # Display the ROC curve plot
-            st.pyplot(plt)
-             
-        # Create a Best Bernoulli Naive Bayes classifier
-        best_bnb_model = tune_hyperparameters_bnb(X_train, y_train)
-        cv_scores = cross_val_score(best_bnb_model, X_train, y_train, cv=5, scoring='accuracy')
-        st.write("Cross-Validation Scores:")
-        st.write(cv_scores)
-        st.write(f"Mean Accuracy: {np.mean(cv_scores)}")
-        st.write(f"Standard Deviation: {np.std(cv_scores)}")
+        # Display the Confusion Matrix
+        st.write("Confusion Matrix:")
+        plt.figure(figsize=(8, 6))
+        sns.heatmap(cf_matrix, annot=labels, fmt='', cmap="Blues", cbar=False,
+                    xticklabels=categories, yticklabels=categories)
+        plt.xlabel("Predicted values", fontdict={'size':14}, labelpad=10)
+        plt.ylabel("Actual values", fontdict={'size':14}, labelpad=10)
+        plt.title("Confusion Matrix", fontdict={'size':18}, pad=20)
+        st.pyplot(plt)
 
-        # Create a Best Multinomial Naive Bayes classifier
-        best_mnb_model = tune_hyperparameters_mnb(X_train, y_train)
-        st.subheader("Evaluation for Multinomial Naive Bayes Model:")
-        model_Evaluate(best_mnb_model)
+        # Compute ROC curve and ROC area for each class
+        fpr = dict()
+        tpr = dict()
+        roc_auc = dict()
+    
+        for i in range(2):
+            fpr[i], tpr[i], _ = roc_curve(y_numerical[:, i], model.predict_proba(X_test)[:, i])
+            roc_auc[i] = auc(fpr[i], tpr[i])
+    
+        # Display the ROC curve
+        st.write("Receiver Operating Characteristic (ROC) Curve:")
+        plt.figure(figsize=(8, 6))
+    
+        for i in range(2):
+            plt.plot(fpr[i], tpr[i], label=f'Class {i} (AUC = {roc_auc[i]:.2f})')
+    
+        plt.plot([0, 1], [0, 1], 'k--', color='grey', linestyle='--', label='Random')
+        plt.xlabel('False Positive Rate')
+        plt.ylabel('True Positive Rate')
+        plt.title('ROC Curve')
+        plt.legend(loc='lower right')
+        plt.grid(True)
         
-        st.subheader("Evaluation for Bernoulli Naive Bayes Model:")
-        model_Evaluate(best_bnb_model)
-        y_pred_original = best_bnb_model.predict(X_test)
+        # Display the ROC curve plot
+        st.pyplot(plt)
+         
+    # Create a Best Bernoulli Naive Bayes classifier
+    best_bnb_model = tune_hyperparameters_bnb(X_train, y_train)
+    cv_scores = cross_val_score(best_bnb_model, X_train, y_train, cv=5, scoring='accuracy')
+    st.write("Cross-Validation Scores:")
+    st.write(cv_scores)
+    st.write(f"Mean Accuracy: {np.mean(cv_scores)}")
+    st.write(f"Standard Deviation: {np.std(cv_scores)}")
+
+    # Create a Best Multinomial Naive Bayes classifier
+    best_mnb_model = tune_hyperparameters_mnb(X_train, y_train)
+    st.subheader("Evaluation for Multinomial Naive Bayes Model:")
+    model_Evaluate(best_mnb_model)
+    
+    st.subheader("Evaluation for Bernoulli Naive Bayes Model:")
+    model_Evaluate(best_bnb_model)
+    y_pred_original = best_bnb_model.predict(X_test)
 
 def sideBar():
     with st.sidebar:
