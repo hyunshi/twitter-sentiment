174f70deb5fcefd09574c6293da91e540dc6e1e8
hyunshi <138978567+hyunshi@users.noreply.github.com>
2024-01-17 00:12:50 +0800

    Update sentiment.py

--- sentiment.py
+++ sentiment.py
@@ -102,9 +102,6 @@ def Home():
 
         df['tweets'] = df['tweets'].apply(lambda x: cleaning_numbers(x))
 
-        # Remove short words
-        df['tweets'] = df['tweets'].apply(lambda x: ' '.join([w for w in x.split() if len(w) > 3]))
-
         # Remove hashtag
         def remove_hashtags(tweets, pattern):
             r = re.findall(pattern, tweets)
