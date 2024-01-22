9f5a10abbfcdb9de4e1ccfc291e488374778fe38
hyunshi <138978567+hyunshi@users.noreply.github.com>
2024-01-22 13:09:18 +0800

    Update sentiment.py

--- sentiment.py
+++ sentiment.py
@@ -346,7 +346,8 @@ def visualize(df):
         # Compute prior and conditional probabilities
         for i in range(N):
             c = y_train[i]
-            V[c].extend(np.where(X_train[i] == 1)[0])  # Update to use numpy's where
+            V[c].extend(np.where(X_train[i] == 1)[0].tolist())  # Update to use numpy's where
+    
             prior[c] += 1
     
         for c in range(N_class):
@@ -357,7 +358,7 @@ def visualize(df):
         # Train classifier
         V_star = set()
         for i in range(X_test.shape[0]):
-            V_star.update(np.where(X_test[i] == 1)[0])  # Update to use numpy's where
+            V_star.update(np.where(X_test[i] == 1)[0].tolist())  # Update to use numpy's where
     
         y_pred = np.zeros(X_test.shape[0])
         for i in range(X_test.shape[0]):
@@ -365,6 +366,7 @@ def visualize(df):
             y_pred[i] = np.argmax(scores)
     
         return y_pred
+
 
     
     def model_Evaluate(model):
